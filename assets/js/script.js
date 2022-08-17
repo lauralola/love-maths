6ef622100f98ba9e256011710f90c482d7d2433d
lauralola <laura6289@gmail.com>
2022-08-17 20:00:03 +0000

    Add correct answer section

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,14 @@ function checkAnswer(){};
 function calculateCorrectAnswer(){
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = parseInt(document.getElementById('operator').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if(operator === '+'){
+        return [operand1 + operand2, 'addition'];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborted`
+    }
 };
 function incrementScore(){};
 function incrementWrongAnswer(){};
