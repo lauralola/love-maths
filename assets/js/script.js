2d8df43c975fc296672badafacb4ab8315179934
lauralola <laura6289@gmail.com>
2022-08-17 19:59:18 +0000

    Added basic java

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName('button');
+    for (let button of buttons) {
+        button.addEventListener('click', function(){
+            if (this.getAttribute('data-type')==='submit'){
+                alert('You clicked submit!!');
+            } else{
+                let gameType = this.getAttribute('data-type');
+                runGame(gameType);
+            }})
+    
+        }
+
+        runGame('addition');
+
+    })
+
+/**
+ * Runs inital set up
+ */
+function runGame(gameType){
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
+
+    if (gameType==='addition'){
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type ${gameType}`)
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+};
+function checkAnswer(){};
+function calculateCorrectAnswer(){
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = parseInt(document.getElementById('operator').innerText);
+};
+function incrementScore(){};
+function incrementWrongAnswer(){};
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
+};
+function displaySubtractQuestion(){};
+function displayMultiplyQuestion(){};
+function displayDivisionQuestion(){};
